37d7bc43d1f2f43e6eb41b4033116e22d33f9d14
IonCiobotari <ivanandrushka@gmail.com>
2019-11-26 18:51:17 +0000

    cantina

--- projeto/cantina.py
+++ projeto/cantina.py
@@ -11,6 +11,9 @@ import requests
 
 app = Flask(__name__)
 
+#@app.route('/')
+#def main():
+
 @app.route('/canteen')
 # menu da semana
 def canteen_week():
@@ -20,14 +23,19 @@ def canteen_week():
     print(r.status_code)
     data = r.json()
     print(data)
+    return str(data)
 
-@app.route('/canteen/<id>')
-def canteed_day():
-    pass
+#dia tem de estar em dd-mm-yyyy
+@app.route('/canteen/<day>')
+def canteed_day(day):
+    day = day.replace("-", "/")
+    print(day)
+    r = requests.get("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/canteen?day="+day)
+    print(r.status_code)
+    data = r.json()
+    print(data)
+    return str(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-# /canteen
-# /canteen/<day>
