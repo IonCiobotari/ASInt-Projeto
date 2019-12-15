4b0e5b9e8b05ddbc9462a7cbb972770e8c28fac7
IonCiobotari <ivanandrushka@gmail.com>
2019-12-15 21:22:29 +0000

    update

--- projeto/appHTML.py
+++ projeto/appHTML.py
@@ -7,14 +7,21 @@ app = Flask(__name__)
 
 APIurl = "http://127.0.0.1:6000/API/"
 
+# main menu
 @app.route('/')
 def main_page():
     return render_template("mainPage.html")
 
+####################################################################
+# canteen microservice
+####################################################################
+
+# canteen main menu
 @app.route('/canteen')
 def canteen():
     return render_template("canteen.html")
 
+# show canteen information for the current week
 @app.route('/canteen_week')
 def canteen_week():
     try:
@@ -24,9 +31,48 @@ def canteen_week():
         r = "ConnectionError"
         return render_template("error_manager.html")
 
+# show canteen information for a specific day
 @app.route('/canteen_day')
 def canteen_day():
-    pass
+    try:
+        r = requests.get(APIurl+"canteen/"+request.args['date'])
+        return render_template("canteen_day.html", result = r.json())
+    except requests.exceptions.ConnectionError:
+        r = "ConnectionError"
+        return render_template("error_manager.html")
+
+
+####################################################################
+# Services microservice
+####################################################################
+
+# show all available services with corresponding ID
+@app.route('/services')
+def services():
+    try:
+        r = requests.get(APIurl+"services")
+        return render_template("services.html", result = r.json())
+    except requests.exceptions.ConnectionError:
+        r = "ConnectionError"
+        return render_template("error_manager.html")
+
+# show all information for a scpecific service
+@app.route('/services_id')
+def service_ID():
+    try:
+        r = requests.get(APIurl+"services/"+request.args['ID'])
+        return render_template("services_ID.html", result = r.json())
+    except requests.exceptions.ConnectionError:
+        r = "ConnectionError"
+        return render_template("error_manager.html")
+
+####################################################################
+# Rooms microservice
+####################################################################
+
+@app.route('/rooms')
+def rooms():
+    return render_template("mainPage.html")
 
 #@app.errorhandler(404)
 #def error_not_found():
