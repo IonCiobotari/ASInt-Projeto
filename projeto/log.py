3649278918a3cc1f604369d69f41cb298e4d98d7
nunokingler <nonuuxx@hotmail.com>
2019-12-23 17:37:51 +0000

    Updated rooms microservice

--- projeto/log.py
+++ projeto/log.py
@@ -9,16 +9,13 @@ app = Flask(__name__)
 logging.config.fileConfig('logging.cfg')
 app.logger = logging.getLogger('defaultLogger')
 
-'''
+
 @app.route('/log', methods=['Post'])
 def logText():
-    #/canteen?day=dd/mm/yyyy para saber menu da semana do dia dd/mm/yyyy
-    #/canteen retorna o menu da semana atual
-
     d= datetime.datetime.today()
     try:
         script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-        rel_path = "Logs/{}{}{}log.txt".format(d.year,d.month,d.day)
+        rel_path = "Logs/LogService.LOG"
         abs_file_path = os.path.join(script_dir, rel_path)
         with open(abs_file_path, 'a+') as file:
             log_text = request.args.get('text')
@@ -27,7 +24,7 @@ def logText():
     except Exception as ex:
         print("Woops something went wrong when logging|{}|{}".format(d,ex))
     return "i didnt do it to em"
-'''
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=4000)
--- projeto/salas.py
+++ projeto/salas.py
@@ -17,12 +17,13 @@ import log
 import requests
 import logging.config
 
-
 app = Flask(__name__)
 
 app.logger = logging.getLogger('defaultLogger')
-#@app.route('/')
-#def main():
+
+
+# @app.route('/')
+# def main():
 
 @app.route('/rooms')
 # menu da semana
@@ -38,36 +39,41 @@ def salas_list():
     return jsonify(r.json())
 
 
-
-#dia tem de estar em dd-mm-yyyy
+# dia tem de estar em dd-mm-yyyy
 @app.route('/rooms/<ID>')
 def salas_ID(ID):
     print(ID)
     try:
-        r = requests.get("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/"+ID)
+        r = requests.get("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/" + ID)
     except requests.exceptions.InvalidURL:
         return jsonify("Fenix service is down")
     print(r.status_code)
     data = r.json()
     print(data)
-    app.logger.info('{} - Processing salas request with ID = {}'.format(request.remote_addr,ID))
+    app.logger.info('{} - Processing salas request with ID = {}'.format(request.remote_addr, ID))
     return jsonify(r.json())
 
-#dia tem de estar em dd-mm-yyyy
+
+# dia tem de estar em dd-mm-yyyy
 @app.route('/rooms/<ID>/<day>')
-def salas_dayID(ID,day):
+def salas_dayID(ID, day):
     day = day.replace("-", "/")
     print(day)
     try:
-        r = requests.get("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/"+ID+'?day='+day)
+        r = requests.get("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/" + ID + '?day=' + day)
     except requests.exceptions.InvalidURL:
         return jsonify("Fenix service is down")
     print(r.status_code)
     data = r.json()
     print(data)
-    app.logger.info('{} - Processing salas request with ID = {} and day = {}'.format(request.remote_addr,ID,day))
-    return jsonify(r.json())#TODO return only ocupation for the currrent day and not for the current week like fenix API returns
-
+    vect = []
+    for event in data['events']:
+        if event['day'] == day:
+            vect.append(event)
+    data['events']= vect
+    app.logger.info('{} - Processing salas request with ID = {} and day = {}'.format(request.remote_addr, ID, day))
+    #return jsonify(r.json())  # TODO return only ocupation for the currrent day and not for the current week like fenix API returns
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5200)
