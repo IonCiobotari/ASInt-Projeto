9e81fa766acfcbeffc53501979b9abb16e5e393a
nunokingler <nonuuxx@hotmail.com>
2019-12-23 17:57:08 +0000

    Added post to log microservice

--- projeto/log.py
+++ projeto/log.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 import datetime
 import os
 
@@ -10,21 +10,34 @@ logging.config.fileConfig('logging.cfg')
 app.logger = logging.getLogger('defaultLogger')
 
 
-@app.route('/log', methods=['Post'])
+@app.route('/log', methods=['GET','POST'])
 def logText():
-    d= datetime.datetime.today()
-    try:
+    if request.method == 'POST':
+        d= datetime.datetime.today()
+        try:
+            script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+            rel_path = "Logs/LogService.LOG"
+            abs_file_path = os.path.join(script_dir, rel_path)
+            with open(abs_file_path, 'a+') as file:
+                log_text = request.args.get('text')
+                file.write("{} -> {}\n".format(d,log_text))
+                return "i did it"
+        except Exception as ex:
+            print("Woops something went wrong when logging|{}|{}".format(d,ex))
+        return "i didnt do it to em"
+    else:
         script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
         rel_path = "Logs/LogService.LOG"
         abs_file_path = os.path.join(script_dir, rel_path)
-        with open(abs_file_path, 'a+') as file:
-            log_text = request.args.get('text')
-            file.write("{} -> {}\n".format(d,log_text))
-            return "i did it"
-    except Exception as ex:
-        print("Woops something went wrong when logging|{}|{}".format(d,ex))
-    return "i didnt do it to em"
-
+        to_json = {}
+        to_json['logs']=[]
+        with open(abs_file_path, 'r') as file:
+            log = file.readline()
+            while log:
+                to_json['logs'].append(log)
+                log = file.readline()
+            #split = log.split('->')
+        return jsonify(to_json)
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
